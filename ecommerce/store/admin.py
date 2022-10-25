246580d8fa67c579f9e74b79bb54bbd5d4c20237
Matthew2720 <mhernandezrpo22@gmail.com>
2022-10-25 13:42:26 -0500

    Finalizacion

--- ecommerce/store/admin.py
+++ ecommerce/store/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Product
+
+class ProductAdmin (admin.ModelAdmin):
+    list_display = ('name','short_description', 'stock',)#Añadir como se visualiza la informacion y campos
+    search_fields = ('name','short_description',) #campo de busqueda
+    list_filter = ('stock','discount_until')  #filtros
+    date_hierarchy = "discount_until" #jerarquia
+
+admin.site.register(Product, ProductAdmin)
+
 
 # Register your models here.
